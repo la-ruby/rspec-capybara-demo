f48985820cbeac2d71f8bf4b6dfd144a1d5b8936
Sam W <sam@sam-we.com>
2021-08-12 06:54:47 -0700

    spec/system/hello_spec.rb

--- spec/system/hello_spec.rb
+++ spec/system/hello_spec.rb
@@ -1,4 +1,4 @@
-Rspec.describe 'Full page example (scrolling)', :type => :system do
+RSpec.describe 'Full page example (scrolling)', :type => :system do
   it 'example' do
     visit 'https://www.whatismybrowser.com/detect/is-javascript-enabled'
     save_and_open_screenshot
