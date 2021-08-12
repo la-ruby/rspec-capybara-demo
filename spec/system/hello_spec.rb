78ef617254d329d73926c82d8086eee720ac2184
Sam W <sam@sam-we.com>
2021-08-12 07:28:39 -0700

    spec/system/hello_spec.rb

--- spec/system/hello_spec.rb
+++ spec/system/hello_spec.rb
@@ -2,14 +2,6 @@ RSpec.shared_context "shared stuff", :shared_context => :metadata do
   before { puts "hereBefore" }
 end
 
-RSpec.shared_examples "some example" do |parameter|
-  let(:foo) { "FOO" }
-
-  it 'something1' do
-    puts "here #{foo}"
-    expect(true).to eq(true)
-  end
-end
 
 
 RSpec.describe 'Full page example (scrolling)', :type => :system do
