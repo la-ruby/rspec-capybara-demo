83c522957afbf55fd4bd48297c36655f913b9721
Sam W <sam@sam-we.com>
2021-08-12 06:37:06 -0700

    spec/system/hello_spec.rb

--- spec/system/hello_spec.rb
+++ spec/system/hello_spec.rb
@@ -3,7 +3,7 @@ require 'byebug'
 describe 'Full page example (scrolling)', :type => :system do
   it 'example' do
     visit 'https://www.whatismybrowser.com/detect/is-javascript-enabled'
-    puts page.text
+    save_and_open_screenshot
   end
 end
 
