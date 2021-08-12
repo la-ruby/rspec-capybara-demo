1b42573ef0993bb0a3ef1668b70b97991c4f3667
Sam W <sam@sam-we.com>
2021-08-12 07:22:35 -0700

    spec/support/matchers/be_aaa_page.rb

--- spec/support/matchers/be_aaa_page.rb
+++ spec/support/matchers/be_aaa_page.rb
@@ -1,7 +1,7 @@
 module BeLoginPage
   extend RSpec::Matchers::DSL
 
-  matcher :be_login_page do
+  matcher :be_aaa_page do
     match_unless_raises do |page|
       expect(page).to have_title "Login"
     end
