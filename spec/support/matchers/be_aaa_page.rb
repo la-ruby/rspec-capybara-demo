3e671e99e5b3cb06b1fbdb452769e7684504d909
Sam W <sam@sam-we.com>
2021-08-12 07:24:02 -0700

    spec/support/matchers/be_aaa_page.rb

--- spec/support/matchers/be_aaa_page.rb
+++ spec/support/matchers/be_aaa_page.rb
@@ -5,5 +5,8 @@ module BeAaaPage
     match_unless_raises do |page|
       expect(page).to have_title "Login"
     end
+    failure_message do |actual|
+      "expected that #{actual} would be a multiple of #{expected}"
+    end
   end
 end
